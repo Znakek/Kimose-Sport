d637974f57c28bce969e28f9762a5451653813e0
Znakek <Znake@server.local>
2014-01-16 08:41:01 +0100

    Start

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+
+<html lang="da">
+<head>
+  <meta charset="utf-8">
+
+  <title>Kimose Sport</title>
+  <meta name="description" content="Boksning">
+  <meta name="author" content="Kimose_Sport">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+</head>
+<body>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
